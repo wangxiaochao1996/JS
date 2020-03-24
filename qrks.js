8c021f175625bf95d8c4244011f5f519ae07825c
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2020-03-24 15:32:04 +0800

    Create qrks.js

---
+++ qrks.js
@@ -0,0 +1,30 @@
+/* 
+#rewrite
+^https://api.weiqire.com/api3/(visitor/|user/unlockCharpter) url script-response-body qrks.js
+#mitm
+hostname = api.weiqire.com
+*/
+
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const path1 = '/api3/visitor/';
+
+if(url.indexOf(path1) != -1){
+  obj.data.enough = 1;}
+else { 
+var reg1 = new RegExp("bid=(\\d+)");
+var reg2 = new RegExp("sort=(\\d+)");
+var bid = url.match(reg1);
+var sort = url.match(reg2);
+obj.status = "success";
+obj.msg = "操作成功";
+obj.code = 1;
+obj.data.bid = parseInt(bid[1]);
+obj.data.sort = [parseInt(sort[1])];
+}
+body = JSON.stringify(obj);
+$done({body});
