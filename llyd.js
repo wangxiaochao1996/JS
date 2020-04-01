2944c74e05f9e9aa0b7253a449fa8f75667f0318
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2020-04-01 14:20:01 +0800

    Update llyd.js

--- llyd.js
+++ llyd.js
@@ -1,26 +1,23 @@
-/*
-流利说•阅读 Surge
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
 
-因缓存机制，使用脚本后可能需要重装一次「流利说•阅读」后方可生效；生效标志是点进详解不再有限制
+const vip = '/api/v2/readings/limitation';
+const time = '/api/v2/readings/accessible';
 
-http-response ^https?:\/\/vira\.llsapp\.com\/api\/v2\/readings\/(accessible|limitation) requires-body=1,max-size=0,script-path=scripts/llyd.js
-
-MitM = vira.llsapp.com
-
-*/
-
-if ($response.status == 200 && $request.method == 'GET') {
-	const bodyObj = JSON.parse($response.body);
-
-	if ($request.url.indexOf('limitation') != -1) {
-		bodyObj.modules = [];
-		bodyObj.auditionDuration = 72000;
-	} else {
-		bodyObj.from = 1482071586
-		bodyObj.to = 1671373986
-	}
-
-	$done({body: JSON.stringify(bodyObj)})
-} else {
-	$done({})
+if (url.indexOf(vip) != -1) {
+	obj["modules"] = [];
+	obj["auditionDuration"] = 7200;
+	body = JSON.stringify(obj);
 }
+
+if (url.indexOf(time) != -1) {
+	obj["from"] = 1482071586;
+	obj["to"] = 1671373986;
+	body = JSON.stringify(obj);
+}
+
+$done({body});
+/**
+ * @supported 9E1BBA07A0EF
+ */
