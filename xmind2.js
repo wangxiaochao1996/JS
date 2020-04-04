fc4cd5273a69a05e7be751786c4faa6cfe8cf158
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2020-04-04 12:49:27 +0800

    Create xmind2.js

---
+++ xmind2.js
@@ -0,0 +1,47 @@
+/*
+
+Quantumult X 脚本:
+Xmind Unlock Annual Subscriptions （by LTribe）
+Xmind Download Link : http://t.cn/A6zhp3Bm
+Thanks To@xiaozhuolao's membership data.
+
+[rewrite_local]
+# Xmind Unlock annual subscriptions （by LTribe）
+https?:\/\/.*\.xmind\..*\/_res\/(devices|user_sub_status|profile\/) url script-response-body XMind.js
+
+[mitm]
+hostname = *.xmind.*,
+
+*/
+
+const path1 = "/devices";
+const path2 = "/user_sub_status";
+const path3 = "/profile/";
+let obj = JSON.parse($response.body);
+
+if ($request.url.indexOf(path1) != -1){
+    obj.license = {
+    "status": "sub",
+    "expireTime": 4102494057000
+  };
+ };
+  
+if ($request.url.indexOf(path2) != -1){
+    obj = {
+    "_code": 200,
+    "ios": {
+    "status": "sub",
+    "expireTime": 4102494057000
+  }};
+ };
+
+if ($request.url.indexOf(path3) != -1){
+  obj.sub = {
+    "ios": {
+      "status": "sub",
+      "expireTime": 4102494057000,
+      "buyable": 0
+    }};
+  };
+  
+$done({body: JSON.stringify(obj)});
