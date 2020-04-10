e5abddfc42121c7b590ba1f2cdc075eb6c8fb924
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2020-04-10 10:00:54 +0800

    Create bubei.js

---
+++ bubei.js
@@ -0,0 +1,21 @@
+/*
+
+Quantumult X 脚本:
+不背单词 unlock （by LTribe）
+Download link : https://t.cn/A6Z1ysfS
+
+[rewrite_local]
+# 不背单词 （by LTribe）
+^https?:\/\/sapi\.beingfine\.cn\/v\d\/report\/launch* url script-response-body bubeidanci.js
+
+[mitm]
+hostname = sapi.beingfine.cn,
+
+*/
+let body = $response.body;
+let obj = JSON.parse(body);
+let ltr = JSON.parse(obj.data_body);
+ltr.privileges.wordroot = {"expire_date":4102429976000,"user_type":2,"granted":1};
+ltr.privileges.collins = {"expire_date":4102429976000,"collins_user_type":2,"granted":1};
+obj.data_body = JSON.stringify(ltr);
+$done({body: JSON.stringify(obj)});
