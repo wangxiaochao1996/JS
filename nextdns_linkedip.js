a01c60a329d2c41fc0a10ebef4e058d1261a8301
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2020-04-26 18:29:30 +0800

    Update nextdns_linkedip.js

--- nextdns_linkedip.js
+++ nextdns_linkedip.js
@@ -1,10 +1,16 @@
 //Script event auto linked ipv4 nextdns: network-change
 
+async function launch() {
+    await linkedip();
+}
+launch()
+function linkedip(){ 
 $httpClient.post('https://link-ip.nextdns.io/5de344/c53f01edaaa9dd9c', function(error, response, data){
   if (error) {
-console.log(error + '‼️');
+console.log('‼️');
   } else {
-console.log(data);
-$done();
+console.log('🟢 '+ data);
   }
+  $done();
 });
+}
