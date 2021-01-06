add965f0bd892582908d7e2d7ad12b1dd268d1cf
wangxiaochao1996 <32659881+wangxiaochao1996@users.noreply.github.com>
2021-01-06 21:10:01 +0800

    Create js.js

---
+++ js.js
@@ -0,0 +1,23 @@
+# > 063 读书笔记 Pro（20210106）
+https://xrckeji.com/union/app/user/query url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/mzjz.js
+
+# > 062 小习惯 Pro（20210106）
+https://xianbeikeji.com/daily/app/user/query url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/mzjz.js
+
+# > 061 萌猪记账 Pro（20210106）
+https://xrckeji.com/itally/app/user/query url script-response-body https://raw.githubusercontent.com/ddgksf2013/Cuttlefish/master/Crack/mzjz.js
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+*/
+var body = $response.body.replace(/vipStatus":0/g, 'vipStatus":1').replace(/vipType":0/g, 'vipType":1').replace(/vipFlag":0/g, 'vipFlag":1');
+$done({ body });
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+*/
+var body = $response.body.replace(/vipStatus":0/g, 'vipStatus":1').replace(/vipType":0/g, 'vipType":1').replace(/vipFlag":0/g, 'vipFlag":1');
+$done({ body });
+/*
+Unlocks by Cuttlefish 公众号：墨鱼手记
+*/
+var body = $response.body.replace(/vipStatus":0/g, 'vipStatus":1').replace(/vipType":0/g, 'vipType":1').replace(/vipFlag":0/g, 'vipFlag":1');
+$done({ body });
